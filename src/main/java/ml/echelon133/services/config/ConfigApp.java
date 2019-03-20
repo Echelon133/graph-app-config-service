2d5cec4ff7b7b5e3acc1c50b3a08ce6a24de6886
Mateusz Bednarski <echelon133@gmail.com>
2019-03-20 14:23:46 +0100

    Add ConfigApp - main class of the config-service

---
+++ src/main/java/ml/echelon133/services/config/ConfigApp.java
@@ -0,0 +1,14 @@
+package ml.echelon133.services.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApp {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigApp.class).run(args);
+    }
+}
